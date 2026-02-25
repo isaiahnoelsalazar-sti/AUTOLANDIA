eaf766f6b08d8631ea7871b5605d7de4a32221ba
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-02-25 19:10:55 +0800

    update script

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const modal = new ECModal({title:"Hello", content:"This is an incomplete system. Any bugs encountered will be fixed in future updates.", buttonAmount:2, buttonLabels:["OK", "Close"]});
+const modal = new ECModal({title:"Note", content:"This is an incomplete system. Any bugs encountered will be fixed in future updates.", buttonAmount:2, buttonLabels:["OK", "Close"]});
 modal.setButtonAction(1, () => {
     modal.setContent("Thank you for your understanding!");
     setTimeout(() => {
         modal.hide();
+        setTimeout(() => {
+            modal.setContent("This is an incomplete system. Any bugs encountered will be fixed in future updates.");
+        }, 200);
     }, 1000);
 });
 modal.setButtonAction(2, () => modal.hide());
 modal.show();
+
+function showNote(){
+    modal.show();
+}
